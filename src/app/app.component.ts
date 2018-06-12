23d1894014761004a732bfc7a94c57628fc02ebb
Suneel Manyam <sunilhari9@gmail.com>
2018-06-12 18:34:04 +0530

    Inti commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {
+    Component
+}
+from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@
+Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = '22227 Paul miller BMW';
+     columnDefs = [
+        {
+            headerName: 'Make',
+            field: 'make'
+        },
+        {
+            headerName: 'Model',
+            field: 'model'
+        },
+        {
+            headerName: 'Price',
+            field: 'price'
+        }
+    ];
+
+    rowData = [
+        {
+            make: 'Toyota',
+            model: 'Celica',
+            price: 35000
+        },
+        {
+            make: 'Ford',
+            model: 'Mondeo',
+            price: 32000
+        },
+        {
+            make: 'Porsche',
+            model: 'Boxter',
+            price: 72000
+        }
+    ];
 }
